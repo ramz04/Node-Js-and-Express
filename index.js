37be35b8817bccc2059171e28cc15cbf56be6537
ramz04 <wunpini95@gmail.com>
2023-11-21 15:53:36 +0000

    update

--- index.js
+++ index.js
@@ -95,7 +95,11 @@
 // })
 
 const express = require("express")
+const morgan = require("morgan")
 const app = express()
+app.use(express.json())
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
 
 const persons = [
   {
@@ -119,3 +123,67 @@ const persons = [
     number: "39-23-6423122",
   },
 ]
+
+const date = new Date()
+app.get("/info", (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p> <br /> <br /> <p>${date}</p>`
+  )
+})
+
+app.get("/", (req, res) => {
+  res.send("<div>Persons Api</div>")
+})
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons)
+})
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find((person) => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter((person) => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post("/api/persons", (req, res) => {
+  body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(404).json({
+      error: "The name or number is missing",
+    })
+  }
+
+  if (body.hasOwnProperty(body.name)) {
+    return res.status(404).json({
+      error: "name must be unique",
+    })
+  }
+
+  const generateId = Math.random() * 100
+
+  const person = {
+    id: generateId,
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
